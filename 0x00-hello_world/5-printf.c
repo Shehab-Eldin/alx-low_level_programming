1d439de1bb26a5175afeb2b6bb199a025f0f236c
Shehab-Eldin <shehab.ahmed9812@gmail.com>
2023-09-07 18:43:25 -0700

    month 0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing using printf function in c
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
