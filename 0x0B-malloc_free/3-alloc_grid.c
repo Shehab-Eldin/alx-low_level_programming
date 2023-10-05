82883460ac6b0a76d5523bdaed7032d34dcc596f
Shehab-Eldin <shehab.ahmed9812@gmail.com>
2023-10-04 17:07:54 -0700

    month1

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid - creates a two dimensional array of ints
+ * @width: width of the matrix
+ * @height: height of the matrix
+ *
+ * Return: pointer to the created matrix (Success)
+ * or NULL (Error)
+ */
+int **alloc_grid(int width, int height)
